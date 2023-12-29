413002b6fa6e6549dc2823cb9796920db81382d4
Hokuto Ide <nehemiaharchive@gmail.com>
2023-12-29 20:18:56 +0900

    Add Statenvertaling Jongbloed 1888 Dutch Bible

--- app/src/main/java/org/gnit/bible/Translation.kt
+++ app/src/main/java/org/gnit/bible/Translation.kt
@@ -64,8 +64,8 @@ enum class Translation(val language: Language, val year: Int, val books: SparseA
 
     // UK already included in English above
     lsg(Language.fr, 1910, Books.FRENCH_NUMBER_NAME_MAP, "Louis Segond Bible", 6),  // lsg,          French
-    sinod(Language.ru, 1876, SparseArray(0)/* TODO implement */, "Синодальный перевод", 7), // СИНОД(sinod), Russian
-    svrj(Language.nl, 1888, SparseArray(0)/* TODO implement */, "Statenvertaling Jongbloed-editie", 8), // SV-RJ(svrj),  Dutch
+    sinod(Language.ru, 1876, Books.RUSSIAN_NUMBER_NAME_MAP, "Синодальный перевод", 7), // СИНОД(sinod), Russian
+    svrj(Language.nl, 1888, Books.DUTCH_NUMBER_NAME_MAP, "Statenvertaling Jongbloed-editie", 8), // SV-RJ(svrj),  Dutch
     rdv24(Language.it, 1924, SparseArray(0)/* TODO implement */, "Revised Diodati Version", 9), // rdv24,        Italian,
 
     // Spain  already included in Spanish above
